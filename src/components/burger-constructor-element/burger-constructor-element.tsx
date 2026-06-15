9eff5f40e9280d9a21cf3030ea4a9496abb94ade
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-16 01:07:39 +0300

    connect store to burger-constructor-element

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,33 @@
 import { FC, memo } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import {
+  constructorSelector,
+  removeFromConstructor,
+  reorderConstructor
+} from '@slices/burger-constructor/burger-constructor-slice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
+    const constructorItems = useSelector(constructorSelector);
 
-    const handleMoveUp = () => {};
+    const handleClose = () => {
+      dispatch(removeFromConstructor(index));
+    };
 
-    const handleClose = () => {};
+    const handleMoveDown = () => {
+      if (index < constructorItems.ingredients.length - 1) {
+        dispatch(reorderConstructor({ from: index, to: index + 1 }));
+      }
+    };
+
+    const handleMoveUp = () => {
+      if (index > 0) {
+        dispatch(reorderConstructor({ from: index, to: index - 1 }));
+      }
+    };
 
     return (
       <BurgerConstructorElementUI
