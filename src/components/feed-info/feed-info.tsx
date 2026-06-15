eeab1e3aef16dc609c72ec5d50f934c72d594a7d
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-16 01:22:34 +0300

    connect store to feed-info

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -1,27 +1,30 @@
 import { FC } from 'react';
-
 import { TOrder } from '@utils-types';
-import { FeedInfoUI } from '../ui/feed-info';
+import { FeedInfoUI } from '@ui';
+import { useSelector } from '../../services/store';
+import {
+  selectFeedOrders,
+  selectFeedTotal,
+  selectFeedTotalToday
+} from '@slices/feed/feed-slice';
 
-const getOrders = (orders: TOrder[], status: string): number[] =>
+const pickOrders = (orders: TOrder[], status: string): number[] =>
   orders
     .filter((item) => item.status === status)
     .map((item) => item.number)
     .slice(0, 20);
 
 export const FeedInfo: FC = () => {
-  /** TODO: взять переменные из стора */
-  const orders: TOrder[] = [];
-  const feed = {};
+  const orders = useSelector(selectFeedOrders);
+  const total = useSelector(selectFeedTotal);
+  const totalToday = useSelector(selectFeedTotalToday);
 
-  const readyOrders = getOrders(orders, 'done');
-
-  const pendingOrders = getOrders(orders, 'pending');
+  const feed = { total, totalToday };
 
   return (
     <FeedInfoUI
-      readyOrders={readyOrders}
-      pendingOrders={pendingOrders}
+      readyOrders={pickOrders(orders, 'done')}
+      pendingOrders={pickOrders(orders, 'pending')}
       feed={feed}
     />
   );
