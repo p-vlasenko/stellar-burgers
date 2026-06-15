93349b2a40032bb05b38a5ec6d8b79d2810ad62c
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-16 01:37:01 +0300

    connect store to forgot password page

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,30 +1,30 @@
 import { FC, useState, SyntheticEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { forgotPasswordApi } from '@api';
 import { ForgotPasswordUI } from '@ui-pages';
+import { useDispatch } from '@store';
+import { forgotPassword, selectAuthError } from '@slices/auth/auth-slice';
 
 export const ForgotPassword: FC = () => {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState('');
-  const [error, setError] = useState<Error | null>(null);
+  const error = useSelector(selectAuthError);
 
   const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
 
-    setError(null);
-    forgotPasswordApi({ email })
-      .then(() => {
-        localStorage.setItem('resetPassword', 'true');
-        navigate('/reset-password', { replace: true });
-      })
-      .catch((err) => setError(err));
+    dispatch(forgotPassword({ email })).then(() => {
+      localStorage.setItem('resetPassword', 'true');
+      navigate('/reset-password', { replace: true });
+    });
   };
 
   return (
     <ForgotPasswordUI
-      errorText={error?.message}
+      errorText={error}
       email={email}
       setEmail={setEmail}
       handleSubmit={handleSubmit}
