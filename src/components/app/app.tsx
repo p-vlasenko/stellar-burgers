543fd698152e59e08cf73002cf6838522ca7b175
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-16 01:44:07 +0300

    fix app launch

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { router } from '../../app/router';
+import { useDispatch } from '@store';
+import { fetchIngredients } from '@slices/ingredients/ingredients-slice';
+import { fetchUser } from '@slices/auth/auth-slice';
 
-const App = () => <RouterProvider router={router} />;
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchIngredients());
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
