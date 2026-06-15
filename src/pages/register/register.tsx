07b8fc15e33f9e4969e5b7fad787b45877ca9238
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-16 01:38:20 +0300

    connect store to reset password page

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,47 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
 
+import { useDispatch } from '@store';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { registerUser, selectAuthError } from '@slices/auth/auth-slice';
+import { useSelector } from 'react-redux';
+
+type LocationState = {
+  from?: {
+    pathname: string;
+  };
+};
+
 export const Register: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const error = useSelector(selectAuthError);
+
+  const state = location.state as LocationState | undefined;
+  const from = state?.from?.pathname ?? '/';
+
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    const action = registerUser({
+      email,
+      password,
+      name: userName
+    });
+
+    dispatch(action).then(() => {
+      navigate(from, { replace: true });
+    });
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={error}
       email={email}
       userName={userName}
       password={password}
--- src/pages/reset-password/reset-password.tsx
+++ src/pages/reset-password/reset-password.tsx
@@ -1,24 +1,24 @@
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { resetPasswordApi } from '@api';
 import { ResetPasswordUI } from '@ui-pages';
+import { resetPassword, selectAuthError } from '@slices/auth/auth-slice';
+import { useDispatch, useSelector } from '@store';
 
 export const ResetPassword: FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
-  const [error, setError] = useState<Error | null>(null);
+  const error = useSelector(selectAuthError);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    setError(null);
-    resetPasswordApi({ password, token })
-      .then(() => {
-        localStorage.removeItem('resetPassword');
-        navigate('/login');
-      })
-      .catch((err) => setError(err));
+
+    dispatch(resetPassword({ password, token })).then(() => {
+      localStorage.removeItem('resetPassword');
+      navigate('/login');
+    });
   };
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export const ResetPassword: FC = () => {
 
   return (
     <ResetPasswordUI
-      errorText={error?.message}
+      errorText={error}
       password={password}
       token={token}
       setPassword={setPassword}
